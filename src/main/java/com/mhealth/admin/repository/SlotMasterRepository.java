98d72502f46a525b13a743079b4b3a74fac8b08e
AvinashP-vivatech <avinash@vivatechrnd.com>
2025-01-29 17:00:24 +0530

    set and get availability

--- src/main/java/com/mhealth/admin/repository/SlotMasterRepository.java
+++ src/main/java/com/mhealth/admin/repository/SlotMasterRepository.java
@@ -9,9 +9,6 @@ import java.util.List;
 
 public interface SlotMasterRepository extends JpaRepository<SlotMaster, Integer> {
 
-    @Query("Select u from SlotMaster u where u.slotType.id= ?1 and u.slotDay = ?2 and u.slotTime in ?3")
+    @Query("Select distinct(u) from SlotMaster u where u.slotType.id= ?1 and u.slotDay = ?2 and u.slotTime in ?3")
     List<SlotMaster> findBySlotTypeIdAndSlotDayAndSlotTimeIn(Integer id, String slotDay, List<String> slotTime);
-
-    @Query("Select u from SlotMaster u where u.slotType.id= ?1 and u.slotDay LIKE ?2 ORDER BY u.slotId ASC")
-    List<SlotMaster> findBySlotTypeIdAndSlotDay(Integer slotTypeId, String date);
 }
