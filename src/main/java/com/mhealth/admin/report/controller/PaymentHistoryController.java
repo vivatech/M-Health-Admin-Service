6ba8cce00a41d40b0d29ba6e9881a00a91601ec9
shamshad <shamshad@vivatechrnd.com>
2025-01-29 15:15:56 +0530

    fix cors

--- src/main/java/com/mhealth/admin/report/controller/PaymentHistoryController.java
+++ src/main/java/com/mhealth/admin/report/controller/PaymentHistoryController.java
@@ -19,10 +19,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -32,6 +29,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @Tag(name = "Payment History", description = "APIs for managing payment history")
+@CrossOrigin(originPatterns = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/admin/payment-history")
 public class PaymentHistoryController {
 
