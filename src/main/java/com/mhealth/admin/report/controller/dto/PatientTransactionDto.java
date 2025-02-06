5cf450028b05657aa97c8ab36da635846ad1edd7
Irfan Ansari <md.irfan@vivatechrnd.com>
2025-02-06 13:29:56 +0530

    Fixed the issue related pagination

--- src/main/java/com/mhealth/admin/report/controller/dto/PatientTransactionDto.java
+++ src/main/java/com/mhealth/admin/report/controller/dto/PatientTransactionDto.java
@@ -1,26 +1,55 @@
 package com.mhealth.admin.report.controller.dto;
 
 import com.mhealth.admin.dto.enums.Channel;
-import com.mhealth.admin.dto.enums.OrderType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Data
 public class PatientTransactionDto {
-
-    private String refId;
-    private String serviceType;
-    private String orderType;
-    private String doctorOrNurse;
-    private String clinicName;
-    private String mobile;
-    private String patientName;
-    private LocalDate age;
-    private String gender;
-    private String patientAddress;
+    // Getters and Setters
+    private Integer userId;
+    private String contactNumber;
+    private String channel;
     private String status;
-    private Channel channel;
-    private LocalDateTime createdAt;
+    private String patientName;
+    private String address;
+    private String refId;
+    private String createdBy;
+    private String orderType;
+    private String dob;
+    private String gender;
+    private String drName;
+    private String clinicName;
+    private String transactionType;
+
+    // Constructor
+    public PatientTransactionDto(Integer userId, String contactNumber, String channel, String status,
+                                 String patientName, String address, String refId, String createdBy,
+                                 String orderType, String dob, String gender, String drName,
+                                 String clinicName, String transactionType) {
+        this.userId = userId;
+        this.contactNumber = contactNumber;
+        this.channel = channel;
+        this.status = status;
+        this.patientName = patientName;
+        this.address = address;
+        this.refId = refId;
+        this.createdBy = createdBy;
+        this.orderType = orderType;
+        this.dob = dob;
+        this.gender = gender;
+        this.drName = drName;
+        this.clinicName = clinicName;
+        this.transactionType = transactionType;
+    }
+
 }
+
--- src/main/java/com/mhealth/admin/repository/SystemTransactionRepository.java
+++ src/main/java/com/mhealth/admin/repository/SystemTransactionRepository.java
@@ -2,57 +2,88 @@ package com.mhealth.admin.repository;
 
 import com.mhealth.admin.model.NodLog;
 import com.mhealth.admin.model.SystemTransaction;
+import com.mhealth.admin.report.controller.dto.PatientTransactionDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface SystemTransactionRepository extends JpaRepository<SystemTransaction, Integer> {
 
-    @Query("""
-    SELECT st
-    FROM SystemTransaction st
-    WHERE (:serviceType IS NULL OR :serviceType = '' OR st.transactionType = :serviceType)
-      AND (st.orderType IS NOT NULL)
-      AND (:status IS NULL OR :status = '' OR st.status = :status)
-      AND (:orderType IS NULL OR :orderType = '' OR st.orderType = :orderType)
-      AND (:channel IS NULL OR :channel = '' OR st.channel = :channel)
-      AND (:fromDate IS NULL OR :fromDate = '' OR st.createdAt >= :fromDate)
-      AND (:toDate IS NULL OR :toDate = '' OR st.createdAt <= :toDate)
-""")
-    Page<SystemTransaction> fetchPatientTransaction(
-            @Param("serviceType") String serviceType,
-            @Param("status") String status,
-            @Param("orderType") String orderType,
-            @Param("channel") String channel,
-            @Param("fromDate") String fromDate,
-            @Param("toDate") String toDate,
-            Pageable pageable
-    );
-    @Query("""
-    SELECT st
-    FROM NodLog st
-    WHERE st.orderType IS NOT NULL
-      AND st.status IS NOT NULL
-      AND (:serviceType IS NULL OR :serviceType = '' OR st.transactionType = :serviceType)
-      AND (:status IS NULL OR :status = '' OR st.status = :status)
-      AND (:orderType IS NULL OR :orderType = '' OR st.orderType = :orderType)
-      AND (:channel IS NULL OR :channel = '' OR st.channel = :channel)
-      AND (:fromDate IS NULL OR :fromDate = '' OR st.createdAt >= :fromDate)
-      AND (:toDate IS NULL OR :toDate = '' OR st.createdAt <= :toDate)
-""")
-    Page<NodLog> fetchNodTransaction(
-            @Param("serviceType") String serviceType,
-            @Param("status") String status,
-            @Param("orderType") String orderType,
-            @Param("channel") String channel,
-            @Param("fromDate") String fromDate,
-            @Param("toDate") String toDate,
-            Pageable pageable
-    );
+    @Query(value = """
+    SELECT st.patient_id, p.country_code, p.contact_number, st.channel, st.status, 
+           p.first_name, p.last_name, p.residence_address, c.name AS city_name, 
+           st.ref_id, st.created_at, st.order_type, p.dob, p.gender, 
+           CONCAT(d.first_name, ' ', d.last_name) AS opp_full_name, 
+           h.clinic_name, st.transaction_type
+    FROM mh_system_transaction st
+    LEFT JOIN mh_users p ON p.user_id = st.patient_id
+    LEFT JOIN mh_consultation mc ON mc.case_id = st.ref_id
+    LEFT JOIN mh_users d ON d.user_id = mc.doctor_id
+    LEFT JOIN mh_users h ON d.hospital_id = h.user_id
+    LEFT JOIN mh_cities c ON c.id = p.city_id
+    WHERE (:transactionType IS NULL OR st.transaction_type = :transactionType)
+      AND (:status IS NULL OR st.status = :status)
+      AND (:orderType IS NULL OR st.order_type = :orderType)
+      AND (:channel IS NULL OR st.channel = :channel)
+      AND (:fromDate IS NULL OR st.created_at >= :fromDate)
+      AND (:toDate IS NULL OR st.created_at <= :toDate)
 
+    UNION ALL 
+
+    SELECT nl.user_id AS patient_id, u.country_code, u.contact_number, 
+           nl.channel, nl.status, u.first_name, u.last_name, u.residence_address, 
+           c.name AS city_name, nl.search_id AS ref_id, nl.created_at, nl.order_type, 
+           u.dob, u.gender, pn.name AS opp_full_name, '' AS clinic_name, 
+           nl.transaction_type
+    FROM mh_nod_logs nl
+    LEFT JOIN mh_users u ON u.user_id = nl.user_id
+    LEFT JOIN mh_dservice_state ds ON nl.search_id = ds.search_id
+    LEFT JOIN mh_partner_nurse pn ON pn.id = ds.nurse_id
+    LEFT JOIN mh_cities c ON c.id = u.city_id
+    WHERE (:transactionType IS NULL OR nl.transaction_type = :transactionType)
+      AND (:status IS NULL OR nl.status = :status)
+      AND (:orderType IS NULL OR nl.order_type = :orderType)
+      AND (:channel IS NULL OR nl.channel = :channel)
+      AND (:fromDate IS NULL OR nl.created_at >= :fromDate)
+      AND (:toDate IS NULL OR nl.created_at <= :toDate)
+
+    ORDER BY created_at DESC
+    """,
+            countQuery = """
+        SELECT COUNT(*) FROM (
+            SELECT st.patient_id FROM mh_system_transaction st
+            WHERE (:transactionType IS NULL OR st.transaction_type = :transactionType)
+              AND (:status IS NULL OR st.status = :status)
+              AND (:orderType IS NULL OR st.order_type = :orderType)
+              AND (:channel IS NULL OR st.channel = :channel)
+              AND (:fromDate IS NULL OR st.created_at >= :fromDate)
+              AND (:toDate IS NULL OR st.created_at <= :toDate)
+
+            UNION ALL
+
+            SELECT nl.user_id FROM mh_nod_logs nl
+            WHERE (:transactionType IS NULL OR nl.transaction_type = :transactionType)
+              AND (:status IS NULL OR nl.status = :status)
+              AND (:orderType IS NULL OR nl.order_type = :orderType)
+              AND (:channel IS NULL OR nl.channel = :channel)
+              AND (:fromDate IS NULL OR nl.created_at >= :fromDate)
+              AND (:toDate IS NULL OR nl.created_at <= :toDate)
+        ) AS total_records
+    """,
+            nativeQuery = true)
+    Page<Object[]> searchWithFilters(
+            @Param("transactionType") String transactionType,
+            @Param("status") String status,
+            @Param("orderType") String orderType,
+            @Param("channel") String channel,
+            @Param("fromDate") String fromDate,
+            @Param("toDate") String toDate,
+            Pageable pageable
+    );
 
 }
